ed0b10c67aee907f9a0872edffdb8d2b3ca7c2f6
Tomo-ja <tomonao.hj@gmail.com>
2022-03-16 18:02:04 -0700

    create header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Header from './compornents/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
---
+++ src/compornents/Header.js
@@ -0,0 +1,8 @@
+export default function Header(){
+	return(
+		<header>
+			<h1>Meme Generator</h1>
+			<p>React Course - Project 3</p>
+		</header>
+	)
+}
