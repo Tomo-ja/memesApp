a6e1daedbd78f33175ba510266acc6a59fc91ce5
Tomo-ja <tomonao.hj@gmail.com>
2022-03-17 11:59:52 -0700

    tempo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './compornents/Header';
+import Meme from './compornents/Meme';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   );
 }
---
+++ src/compornents/Meme.js
@@ -0,0 +1,11 @@
+export default function Meme (){
+	return(
+		<div className="meme">
+			<form action="#">
+				<input type="text" placeholder="Shut up" />
+				<input type="text" placeholder="and take my money" />
+				<input className="submit" type="submit" value="Get a new meme image" />
+			</form>
+		</div>
+	)
+}
